80de92071c7dd7a5b9935566312f8126b0f52c5c
aminelli <antonio.minelli@etlforma.com>
2026-02-06 17:07:59 +0100

    upd

---
+++ Install/02-microk8s/script-01-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo snap install microk8s --classic
+microk8s status --wait-ready
+
+# Enable Addons
+microk8s enable community
+microk8s enable dashboard
+microk8s enable dns 
+microk8s enable rbac
+microk8s enable ingress
+microk8s enable hostpath-storage
+microk8s enable registry
+microk8s enable metallb
+microk8s enable cert-manager
+microk8s enable istio
